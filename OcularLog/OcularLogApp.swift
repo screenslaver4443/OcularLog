186cc4d1176568f386b0121fa480d3dac9b4404f
Nikolai <57935446+screenslaver4443@users.noreply.github.com>
2023-10-19 10:17:43 +1030

    Initial Commit

---
+++ OcularLog/OcularLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  OcularLogApp.swift
+//  OcularLog
+//
+//  Created by Nikolai Pesudovs on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct OcularLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
