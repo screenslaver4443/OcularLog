db1eee12c9e84b806b4a1dba351e495af124c9b6
Nikolai <57935446+screenslaver4443@users.noreply.github.com>
2023-10-24 11:50:41 +1030

    Fixed dumb bundle identifier error and started on home page

--- OcularLog/ContentView.swift
+++ OcularLog/ContentView.swift
@@ -9,16 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (alignment: .leading) {
+            Text("Today")
+                .font(.largeTitle)
+                .bold()
+            VStack() {
+                Text("PlaceHolder")
+            }
+                .frame(width: 350, height: 300)
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 25).stroke(.black, lineWidth: 10).fill(.cyan).frame(width: 350, height: 300))
         }
         .padding()
+        
     }
 }
-
 #Preview {
     ContentView()
 }
