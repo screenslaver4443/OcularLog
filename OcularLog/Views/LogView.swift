bdfedf1a3ab1e2fed4318e78684e7a9d7302e244
Nikolai <57935446+screenslaver4443@users.noreply.github.com>
2023-10-27 22:20:31 +1030

    Enabled cycling through questions to the end of the questions array. Ready for question selecting algorithm

--- OcularLog/Views/LogView.swift
+++ OcularLog/Views/LogView.swift
@@ -7,28 +7,26 @@
 
 import SwiftUI
 
-let questions = ["Placeholder"]
-let imageNames = ["Reading"]
-var questionNumber = 0
-
-
 struct LogView: View {
-    @State private var questiontext = questions[questionNumber]
-    @State private var answers : [Int] = []
-    @State private var unroundedanswer: Double = 0
-
+    let questions = ["How much difficulty are you having reading?", "Difficulty seeing in the distance?", "Recognising faces across the street?", "Watching tv?", "Seeing in bright light?", "Seeing in poor light?", "Appreciating colours?", "Driving a car during the day?", "Driving a car during the night?", "Walking inside?", "Walking outside?", "Using steps & stairs?", "Crossing the road?", "Using public transport?", "Travelling independently?", "Moving in unfamilair surroundings?", "Employment/housework/education activities?", "Hobbies/leisure activities?"]
+    let imageNames = ["Reading"]
+    @State var questionNumber = 0
+    @State var answers : [Int] = []
+    @State var unroundedanswer: Double = 0
+    
     var body: some View {
         VStack(){
             VStack(){
-                Text(questiontext)
+                Text(questions[questionNumber])
                     .font(.largeTitle)
                     .bold()
-                Image(imageNames[questionNumber])
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .padding()
+//                Image(imageNames[questionNumber])
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fit)
+//                    .frame(minWidth: 200, maxWidth: 300, minHeight: 250, maxHeight: 350)
+//                    .padding()
                 HStack(){
-                    Text("Not at All")
+                    Text("None at All")
                     Slider(value: $unroundedanswer,
                            in: 0 ... 4,
                            step: 1)
@@ -40,10 +38,25 @@ struct LogView: View {
                     .foregroundColor(.blue)
                     .font(.title2)
                 
+                Button(action: {submit(answer: Int(unroundedanswer.rounded()))}){
+                    Text("Submit Answer")
+                }
+                .buttonStyle(.bordered)
+                
             }
             Spacer()
         }
         
+        
+    }
+    func submit(answer: Int){
+        answers.append(answer)
+        chooseQuestion(answer: answer)
+    }
+
+    func chooseQuestion(answer: Int){
+        //Placeholder until scoring system is known
+        questionNumber += 1 //Will cause a crash if number exceeds questions array
     }
 }
 
